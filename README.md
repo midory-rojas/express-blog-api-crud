57c79a1fc7812c8b479bb49fe75d0f41bbac2a32
“midory-rojas” <midory.rojasv@gmail.com>
2025-12-18 17:55:42 +0100

    feat:impostazione della logica della funzione destroy ed anche il controllo con il messagio di ogni operazione

--- README.md
+++ README.md
@@ -6,3 +6,25 @@
 - Testare su postman
 
 # Esercizio:
+
+## Milestone 1
+
+- [x] Come prima cosa, creiamo un controller per i post, in una cartella controllers. 
+
+ - [x] All’interno, preparo tutte le funzioni necessarie e copio in ciascuna la logica delle funzioni che attualmente si trovano nel router (al momento restituiscono solo dei messaggi). 
+
+ - [x] Poi torno sul file delle rotte. Qui importo le funzioni dichiarate nel controller e le associo alle varie rotte.
+
+-  [x] Faccio il test su postman se chiamando gli endpoint ricevo effettivamente le stesse risposte che avevo prima. 
+
+-  [x] Se tutto funziona, passo alla prossima milestone.
+
+## Milestone 2
+
+
+-  [x] Implemento le logiche del CRUD:
+
+-  [x] INDEX dovrà restituire la lista dei post in formato JSON
+-  [x] SHOW dovrà restituire un singolo post in formato JSON
+-  [x] DESTROY dovrà eliminare un singolo post dalla lista, stampare nel terminale (console.log) la lista aggiornata, e rispondere con uno stato 204 e nessun contenuto.
+-  [x] In SHOW e DESTROY, controllo se il parametro si riferisce ad un post esistente, in caso contrario, risponde con uno stato 404 e un messaggio d’errore, sempre in formato JSON.
--- controllers/postController.js
+++ controllers/postController.js
@@ -4,37 +4,37 @@ import caniArray from "../data/cani.js";
 // Lettura di tutti i post dei cani - INDEX
 function index(req, res) {
     const risposta = {
-        count:caniArray.length,
+        count: caniArray.length,
         results: caniArray,
     };
     res.json(risposta);
 }
 
 // Dettagli di un singolo post (cane) - SHOW
-function show(req,res) {
-   const id = parseInt(req.params.id);
-   //uso find 
-   const cane = caniArray.find((cane) => cane.id === id);
-   //uso la condizione
-   if (cane !== undefined) {
-    res.json(cane);
-   } else {
-    res.status(404); //Status 404
-    res.json({
-        error: "Not found",
-        message: "Cane non trovato",
-    });
-   }
+function show(req, res) {
+    const id = parseInt(req.params.id);
+    //uso find 
+    const cane = caniArray.find((cane) => cane.id === id);
+    //uso la condizione
+    if (cane !== undefined) {
+        res.json(cane);
+    } else {
+        res.status(404); //Status 404
+        res.json({
+            error: "Not found",
+            message: "Cane non trovato",
+        });
+    }
 }
 
 // Creare un nuovo tipo di post (cane) - STORE
-function store(req,res) {
+function store(req, res) {
     res.send("creo un nuovo tipo di post di cane");
 }
 
 
 // Modificare un post di cane esistente - UPDATE
-function update(req,res) {
+function update(req, res) {
     const id = parseInt(req.params.id);
     res.send(`modifica un post di cane esistente ${id}`);
 }
@@ -48,7 +48,28 @@ function modify(req, res) {
 // Cancellare un post di cane esistente - DESTROY
 function destroy(req, res) {
     const id = parseInt(req.params.id);
-    res.send(`cancela il post di cane ${id}`);
+
+    //Implemntazione della logica 
+    //1. Trovare index del post di cani con findIndex
+    const caneIndex = caniArray.findIndex((cane) => cane.id === id);
+    //console.log(caneIndex);
+
+    //Condixione
+
+    if (caneIndex === -1) {
+        res.status(404); //Status 404 not found
+        res.json({
+            error: "not found",
+            message: "Cane non trovato",
+        });
+    } else {
+        //2. Cancello il post di cane con lo splice
+        //partendo dal indice 1, cancello un'elemento (1)
+        //dopo faccio il test con index per vedere si è stato cancelato il post
+
+        caniArray.splice(caneIndex, 1)
+        res.sendStatus(204) //Status 204 non c'è nessun contenuto
+    }
 }
 
 
