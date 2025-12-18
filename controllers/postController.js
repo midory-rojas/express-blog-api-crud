68567ade0d8ee30842673fd5be5d6a7523a4a459
“midory-rojas” <midory.rojasv@gmail.com>
2025-12-18 16:52:30 +0100

    feat: ho impostato la logica sulla prima operazione index

--- controllers/postController.js
+++ controllers/postController.js
@@ -10,25 +10,31 @@ function index(req, res) {
     res.json(risposta);
 }
 
+// Dettagli di un singolo post (cane) - SHOW
 function show(req,res) {
    const id = parseInt(req.params.id);
    res.send(`dettagli di un singolo post di cane ${id}`);
 }
 
+// Creare un nuovo tipo di post (cane) - STORE
 function store(req,res) {
     res.send("creo un nuovo tipo di post di cane");
 }
 
+
+// Modificare un post di cane esistente - UPDATE
 function update(req,res) {
     const id = parseInt(req.params.id);
     res.send(`modifica un post di cane esistente ${id}`);
 }
 
+// Modificare solo alcuni dati di un post di cane esistente - MODIFY
 function modify(req, res) {
     const id = parseInt(req.params.id);
     res.send(`modifica parzialmente il post di cane ${id}`);
 }
 
+// Cancellare un post di cane esistente - DESTROY
 function destroy(req, res) {
     const id = parseInt(req.params.id);
     res.send(`cancela il post di cane ${id}`);
