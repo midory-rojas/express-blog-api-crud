dd41fef77a19292d41e6cdfb2adf5807e6e820e7
“midory-rojas” <midory.rojasv@gmail.com>
2025-12-18 16:02:13 +0100

    feat: creo la porta e le cartelle di controllers, data, public e routers

---
+++ README.md
@@ -0,0 +1,8 @@
+# Express Blog - API CRUD (parte 1)
+
+# Dipendenze:
+
+- Express
+- Testare su postman
+
+# Esercizio:
--- app.js
+++ app.js
@@ -1,7 +1,18 @@
+//Importo express
 import express from "express";
 
+//Invoco express
 const app = express();
+
+//Creo la porta 3000
 const port = 3000;
+
+//Creo una cartella public per inserire le immagine e accedere facilmente dal browser
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.send("Benvenuti al blog dei cani più belli e amorevooli");
+})
 
 app.listen(port, () => {
     console.log(`Íl server è partito sulla porta ${port}`);
